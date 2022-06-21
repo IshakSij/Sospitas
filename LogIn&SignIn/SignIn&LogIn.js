4b9b17e93a89b917421788a2d6a112b00b609c4f
Tobi Oduniyi <oluwatobi.oduniyi@stud.fh-campuswien.ac.at>
2022-06-21 22:20:06 +0200

    LogIn and SignUP

---
+++ LogIn&SignIn/SignIn&LogIn.js
@@ -0,0 +1,26 @@
+const   forms = document.querySelector('.forms'),
+    pwShowHide = document.querySelector('.eye-icon'),
+    links = document.querySelector('.eye-icon');
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password")
+
+        pwFields.forEach(password => {
+            if (password.type === "password") {
+                password.text = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+            }
+            password.type = "passeord";
+            eyeIcon.classList.replace("bx-show", "bx-hide")
+        })
+    })
+
+})
+
+links.forEach(link => {
+    link.addEventListener("click", e =>{
+        e.preventDefault(); //preventing form submit
+        forms.classList.toggle("show-signup")
+    })
+})
